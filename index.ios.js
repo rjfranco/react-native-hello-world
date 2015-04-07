ac52c448ccc4cd3149b74bb84f8183a2b13b728e
Ramiro Jr. Franco <rjfranco@gmail.com>
2015-04-06 23:08:43 -0700

    First, and possibly only commit from me playing around with react native.

---
+++ index.ios.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <Text style={styles.welcome}>
+            Hello World
+          </Text>
+        </View>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    color: '#fff',
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  box: {
+    padding: 20,
+    backgroundColor: 'green'
+  }
+});
+
+AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
